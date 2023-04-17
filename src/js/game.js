923d6d39620a4132dc84dece50c1afeff708a84f
nitay16 <103889257+nitay16@users.noreply.github.com>
2023-04-17 23:40:48 +0300

    finished movement of enemy spaceships

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,29 @@ space_ship_img = new Image()
 enemy_space_ship_img =new Image()
 space_ship_fire = new Image()
 enemy_space_ship_fire = new Image()
+let TIME_INTERVAL=25;//screen refresh interval in milliseconds
+var canvasWidth; // width of the canvas
+var canvasHeight; // height of the canvas
+
+// section for creating matrix of enemy spaces
+enemy_spaceship_2d_array = new Array(4);
+for (let i = 0; i < enemy_spaceship_2d_array.length; i++) {
+    enemy_spaceship_2d_array[i] = new Array(5);
+}
+  
+  
+// Loop to initialize 2D array elements.
+for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 5; j++) {
+        enemy_spaceship_2d_array[i][j] = 1;
+    }
+}
+
+
+
+
+
+
 
 //random location for the spaceship
 location_x_player = Math.floor(Math.random() * 400) + 100;
@@ -16,6 +39,9 @@ player_area = 1 - 0.4;
 var player_laser_param = {x:-1,y:-1,w:40,h:40}
 var canvas;
 var context;
+var enemy_state;
+
+
 //func for moving the space ship
 function move_space_left() {
     // TODO need to add check that the player is not leving the map
@@ -54,12 +80,47 @@ function move_space_down() {
         requestAnimationFrame(draw)
     }
 }
+function startTimer()
+{
+//    canvas.addEventListener( "click", fireCannonball, false );
+   intervalTimer = window.setInterval( updatePositions, TIME_INTERVAL);
+} // end function startTimer
+function updatePositions()
+{
+    // update the Enemy's position
+   var targetUpdate = TIME_INTERVAL / 1000.0 * target_velocity;
+   enemy_state.start += targetUpdate;
+
+   if (enemy_state.start < 0 || enemy_state.start+5*70 > canvas.width){
+    target_velocity *= -1;
+   }
+
+//    target.end.y += targetUpdate;
+    draw();
+}
+
+function draw_enemy(img_enemy){
+      
+    // Loop to display the elements of 2D array. 
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 5; j++)    {
+            if(enemy_spaceship_2d_array[i][j]==1){
+            contex.drawImage(img_enemy,enemy_state.start+70*j ,0+70*i ,70,70);
+            }
+        }
+    } 
+
+}
+
 //func that do the draw on the game screen
 // IMPORTANT for add enemy and other drawing , need to add them to the func
 function draw() {
     // draw the spae ship
     contex.clearRect(0,0,canvas.width,canvas.height)
     contex.drawImage(space_ship_img, location_x_player, location_y_player,70,70)
+    draw_enemy(enemy_space_ship_img);
+    // contex.drawImage(enemy_space_ship_img, canvas.width*5/8, canvas.height/8,70,70);
+
 
     // requestAnimationFrame(move)
     // draw the laser
@@ -85,17 +146,31 @@ function player_fire() {
     requestAnimationFrame(draw)
 }
 
-
 // func for the start of the game and add event listener for the keyboard
 function start_game_after_config() {
+    enemy_state= new Object();
+    enemy_state.start=0;
     space_ship_fire.src = "Resource/imgs/effects/laser.png"
     enemy_space_ship_fire.src = "Resource/imgs/effects/laser_enemy.png"
     enemy_space_ship_img.src = "Resource/imgs/spaceship/enemy_spaceship.png"
     laser = new Object();
     space_ship_img.src = get_spaceship()
     canvas = document.getElementById("Game");
+    var w= canvas.width;
+    var h= canvas.height;
+    canvasWidth = w; // store the width
+    canvasHeight = h; // store the height
+    
+    initialTargetVelocity = -h / 4; // initial target speed multiplier
     contex = canvas.getContext("2d");
+    // reset_elements();
     contex.drawImage(space_ship_img, location_x_player, location_y_player,70,70);
+    draw_enemy(enemy_space_ship_img);
+    target_velocity=initialTargetVelocity;
+    startTimer();
+    
+    
+
     document.addEventListener('keydown' ,function () {
         key = event.keyCode;
         switch (key) {
